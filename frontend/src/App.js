bedefac44d1bbd5775739c293498e30080b76d9f
박승희 <pshee0329@naver.com>
2023-08-04 10:40:22 +0900

    Feat: App.js 라우터 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Map from './pages/MapPage';
+import ZzimList from './pages/ZzimList';
+import RoomOut from './pages/RoomOut';
+import Signuplogin from './pages/Signuplogin';
+import GbbList from './pages/GbbList';
+import GbbCreate from './pages/GbbCreate';
+import Main from './pages/Main'
+import Auth from './pages/Auth';
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 
-function App() {
+const App = () => {
+  const [imageList, setImageList] = useState([]);
+
+  const handleImageUpload = (images) => {
+    setImageList([...imageList, images]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/gbblist" element={<GbbList imageList={imageList} />} />
+          <Route path="/gbbcreate" element={<GbbCreate onImageUpload={handleImageUpload} />} />
+          <Route path="/zzim" element={<ZzimList />} />
+          <Route path="/roomout" element={<RoomOut />} />
+          <Route path="/login" element={<Signuplogin />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
+
+
