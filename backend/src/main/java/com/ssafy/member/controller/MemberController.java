e13b346f931b01e531a8aab6c20b68e94b5e97d4
jihye byun <jhye.byun@gmail.com>
2023-08-08 17:49:29 +0900

    Refactor: 카카오 로그인 requestDto 수정

--- backend/src/main/java/com/ssafy/member/controller/MemberController.java
+++ backend/src/main/java/com/ssafy/member/controller/MemberController.java
@@ -28,16 +28,16 @@ public class MemberController {
      * 카카오 로그인 요청을 처리한다.
      * 곰방 로그인의 경우 회원 정보를 반환하고, 회원가입의 경우와 회원가입 이후 사용자 이름을 입력하지 않은 경우 UUID를 반환한다.
      *
-     * request_url=https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=a20ef37212e1ae86b20e09630f6590ce&redirect_uri=http://localhost:8080/member/login
-     * @param request 카카오 인가토큰이 포함된 request를 입력받는다.
+     * request_url=https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=a20ef37212e1ae86b20e09630f6590ce&redirect_uri=http://localhost:3000/auth
+     * @param kakaoLoginRequestDto 카카오 인가토큰을 입력받는다.
      * @return 로그인하는 경우 : 회원 정보를 반환한다.
      * @return 회원가입하는 경우 : 새롭게 생성된 UUID를 반환한다.
      * @return 회원가입 후 사용자 이름을 입력하지 않은 경우 : 회원가입 시 생성된 UUID를 반환한다.
      */
     @GetMapping("/login")
-    public BaseResponse<Object> loginCallBack(HttpServletRequest request) {
+    public BaseResponse<Object> loginCallBack(@RequestBody KakaoLoginRequestDto kakaoLoginRequestDto) {
         // 인가코드로 토큰 발급
-        KakaoTokenResponseDto kakaoToken = memberService.getKaKaoToken(request.getParameter("code"));
+        KakaoTokenResponseDto kakaoToken = memberService.getKaKaoToken(kakaoLoginRequestDto.getCode());
         // 토큰으로 카카오 사용자 정보 요청 (channelId, email, gender)
         KakaoLoginResponseDto kakaoLoginResponseDto = memberService.getMemberInfoWithToken(kakaoToken);
         MemberLoginResponseDto memberLoginResponseDto = null;
@@ -45,7 +45,6 @@ public class MemberController {
         try {
             // DB에 존재하는 사용자면 로그인
             memberLoginResponseDto = new MemberLoginResponseDto(memberService.getMemberInfoWithChannelId(kakaoLoginResponseDto.getId()));
-            System.out.println(memberLoginResponseDto.toString());
             // 회원가입은 됐지만 이름 입력이 안된 사용자면 UUID와 함께 이름 입력 화면으로 반환
             if (memberLoginResponseDto.getMember().getName().equals("사용자"))
                 throw new IllegalAccessException();
@@ -53,12 +52,10 @@ public class MemberController {
         } catch (NoSuchElementException e) {
             // DB에 존재하지 않는 사용자면 회원가입
             MemberJoinResponseDto memberJoinResponseDto = new MemberJoinResponseDto(memberService.join(kakaoLoginResponseDto).getId());
-            System.out.println(memberJoinResponseDto.toString());
             return responseService.getSuccessResponse("회원가입 성공", memberJoinResponseDto);
         } catch (IllegalAccessException e) {
             // DB에 가입했지만 이름을 입력하지 않은 사용자면 UUID만 반환해서 이름 입력 창으로 전환
             MemberJoinResponseDto memberJoinResponseDto = new MemberJoinResponseDto(memberLoginResponseDto.getMember().getId());
-            System.out.println(memberJoinResponseDto.toString());
             return responseService.getSuccessResponse("이름 입력 안된 사용자", memberJoinResponseDto);
         }
     }
@@ -72,7 +69,6 @@ public class MemberController {
     @PostMapping("/update")
     public BaseResponse<Object> updateName(@RequestBody MemberUpdateRequestDto memberUpdateRequestDto) {
         MemberLoginResponseDto memberLoginResponseDto = new MemberLoginResponseDto(memberService.updateMemberName(memberUpdateRequestDto));
-        System.out.println(memberLoginResponseDto);
         return responseService.getSuccessResponse("이름 수정 성공", memberLoginResponseDto);
     }
 }
