9c26d6848d706788d8ae057d9fbd357fd9afc0c1
Chae Huichan <eqholic1125@gmail.com>
2023-08-09 10:31:18 +0900

    Feat: ShowRoom 등록 API 구현

---
+++ backend/src/main/java/com/ssafy/showRoom/dto/ShowRoomResponseDto.java
@@ -0,0 +1,12 @@
+package com.ssafy.showRoom.dto;
+
+import com.ssafy.showRoom.domain.ShowRoom;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ShowRoomResponseDto {
+
+    private ShowRoom showRoom;
+}
