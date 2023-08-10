fd9cc372666a23c02dddaf37f58691ccb38a6a2b
HyundoLee <hyundo1995@gmail.com>
2023-08-10 14:05:32 +0900

    Feat: 카카오 로그인 API 적용

--- frontend/src/pages/Signuplogin.js
+++ frontend/src/pages/Signuplogin.js
@@ -1,23 +1,14 @@
-
 import "./Signuplogin.css";
 const Loginpage = () => {
 
   const Rest_api_key='a20ef37212e1ae86b20e09630f6590ce' //REST API KEY
-  const redirect_uri = 'http://localhost:8080/member/login' //Redirect URI to backend
+  const redirect_uri = 'http://localhost:3000/auth' //Redirect URI to backend
   // oauth 요청 URL
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`
-  // const handleLogin = ()=>{
-  //     window.location.href = kakaoURL
-  // }
+  const handleLogin = ()=>{
+      window.location.href = kakaoURL
+  }
 
-  const logintest = async ()=>{
-    // 카카오 사용자 정보 조회
-    await axios.get(kakaoURL)
-    .then(function(r){
-        console.log(r.data)
-    })
-    .catch(function(error){console.error('error',error)})
-}
 
   return (
     <div className="wrapper">
@@ -31,7 +22,7 @@ const Loginpage = () => {
                   className="kakaologo" 
                   src="/assets/kakao_login_large_wide.png" 
                   alt="카카오 로그인 버튼" 
-                  onClick={logintest} 
+                  onClick={handleLogin}
                 />
 
             </div>
