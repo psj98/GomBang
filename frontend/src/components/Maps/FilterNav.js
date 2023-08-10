21313d672b34d657b36c67f9b0b4d7904930ff40
HyundoLee <hyundo1995@gmail.com>
2023-08-10 14:17:59 +0900

    Feat: 매물 검색 필터 UI

--- frontend/src/components/Maps/FilterNav.js
+++ frontend/src/components/Maps/FilterNav.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './FilterNav.module.css';
-
+import ModalPortal from '../ModalPortal'
+import FilterAdjust from './FilterAdjust'
 const FilterNav = () => {
+  const [showmodal,setshowmodal] = useState(false)
+  const handleOpen = (event) =>{
+    setshowmodal(true)
+  }
+  const handleClose = (event) =>{
+    event.preventDefault();
+    if(event.target === event.currentTarget)
+    {
+      setshowmodal(false)
+    }
+  }
+
   return (
     <div className={styles.Frame}>
       <input className={styles.LocSearch} type="text" placeholder='지역, 학교, 역으로 검색'/>
-      {/* <hr className={styles.verticalLine} /> */}
       <input className={styles.ContSearch} type="text" placeholder='검색어를 입력하세요'/>
-      <div className={styles.icon_parents}>
-        상세 필터
-        <img className={styles.icon_detail}  src="/assets/detail.svg" alt="" />
-        {/* <img className={styles.icon_detail}  src="/assets/filterremove.svg" alt="" /> */}
+      <div className={styles.icon_parents} >
+        <span onClick={handleOpen}>상세 필터</span>
+        <img className={styles.icon_detail}  src="/assets/detail.svg" alt="" onClick={handleOpen}/>
+        <img className={styles.icon_detail}  src="/assets/filterremove.svg" alt="" />
       </div>
+      {showmodal && (
+        <ModalPortal closePortal={handleClose}>
+          <FilterAdjust />
+        </ModalPortal>
+      )}
     </div>
   )
 }
