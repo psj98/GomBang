59a934bf5c61a6a07bd9751bed1c5a24ef7d8624
psj98 <park98sj1@hanmail.net>
2023-08-12 03:41:27 +0900

    Refactor: 어노테이션 추가 (Repository)

--- backend/src/main/java/com/ssafy/elasticsearch/repository/RoomDealElasticSearchRepository.java
+++ backend/src/main/java/com/ssafy/elasticsearch/repository/RoomDealElasticSearchRepository.java
@@ -2,6 +2,8 @@ package com.ssafy.elasticsearch.repository;
 
 import com.ssafy.elasticsearch.dto.RoomDealSaveDto;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoomDealElasticSearchRepository extends ElasticsearchRepository<RoomDealSaveDto, String> {
 }
