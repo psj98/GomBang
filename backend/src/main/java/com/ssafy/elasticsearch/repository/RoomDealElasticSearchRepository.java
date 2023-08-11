a82c41331958d917583ea6f3cfac10c90ec85b01
psj98 <park98sj1@hanmail.net>
2023-08-12 02:44:25 +0900

    Refactor: import Dto명 변경

--- backend/src/main/java/com/ssafy/elasticsearch/repository/RoomDealElasticSearchRepository.java
+++ backend/src/main/java/com/ssafy/elasticsearch/repository/RoomDealElasticSearchRepository.java
@@ -1,7 +1,7 @@
 package com.ssafy.elasticsearch.repository;
 
-import com.ssafy.elasticsearch.dto.RoomDealSearchResponseDto;
+import com.ssafy.elasticsearch.dto.RoomDealSaveDto;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-public interface RoomDealElasticSearchRepository extends ElasticsearchRepository<RoomDealSearchResponseDto, String> {
+public interface RoomDealElasticSearchRepository extends ElasticsearchRepository<RoomDealSaveDto, String> {
 }
