90a607f9604e2b0a90c815da397ac89ebed00235
박승희 <pshee0329@naver.com>
2023-08-14 17:43:11 +0900

    곰방봐 백엔드 api 연결

--- frontend/src/pages/GbbCreate.js
+++ frontend/src/pages/GbbCreate.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import { useNavigate   } from 'react-router-dom';
 import Header from '../components/Header';
+import styles from '../components/Gbb.module.css'
 
 const Create = ({ onImageUpload }) => {
     const [selectedImages, setSelectedImages] = useState([]);
     const [hashtag, setHashtag] = useState('');
     const [selectedHashtags, setSelectedHashtags] = useState([]);
-
-        const navigate = useNavigate  ();
+    const navigate = useNavigate  ();
+    const apiUrl = process.env.REACT_APP_API_ROOT; // 백엔드 서버의 주소
+    const uploadUrl = `${apiUrl}/show/add`;
         
         const handleImageChange = (event) => {
         const files = event.target.files;
@@ -30,50 +32,73 @@ const Create = ({ onImageUpload }) => {
     };
     
 
-    const handleUpload = () => {
+    const handleUpload = async () => {
         if (selectedImages.length > 0 || hashtag.trim() !== '') {
-        const newImageGroup = {
-            hashtag: selectedHashtags,
-            images: selectedImages,
-        };
-
-        // 이미지들을 로컬에 저장하고 리스트에 추가하는 함수 호출
-        // onImageUpload({ hashtag, images: selectedImages });
-        onImageUpload(newImageGroup);
-        setSelectedImages([]);
-        setHashtag('');
-        navigate('/gbblist');
-        // navigate('/list', {setSelectedImages});
+            const newImageGroup = {
+                room_deal_id: 123, // 실제 room_deal_id로 변경해야 합니다
+                user_id: 'user123', // 실제 user_id로 변경해야 합니다
+                images: selectedImages.map((image) => ({ src: URL.createObjectURL(image) })),
+                tags: selectedHashtags.map((tag) => ({ tag_name: tag })),
+            };
+            try {
+                const response = await fetch( uploadUrl, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(newImageGroup),
+                    });
+            
+                    if (response.ok) {
+                    // 성공 시, 필요한 동작을 수행합니다
+                    setSelectedImages([]);
+                    setHashtag('');
+                    navigate('/gbblist');
+                    } else {
+                    // 에러 처리
+                    console.error('에러:', response.status);
+                    }
+                } catch (error) {
+                    console.error('에러:', error);
+                }
         }
     };
 
     return (
         <div>
             <Header />
-        <div>
-            <input
-                type="file"
-                accept="image/*"
-                multiple
-            onChange={handleImageChange} />
-        
-        </div>
-        <div>
-            <label>해시태그:</label>
-                <input
-                    type="text"
-                    value={hashtag}
-                    onKeyDown={handleKeyDown}
-                    onChange={handleHashtagChange}
-                    placeholder='# 해시태그를 입력하세요'
-                />
+            <div className={styles.gbbcreateform}>
+                <div className={styles.h3}>곰방봐 올리기</div>
+                <div className={styles.inputbox}>
+                    <div className={styles.imageinput}
+                     onClick={() => document.getElementById('image-upload').click()}> +
+                    <input
+                        id="image-upload"
+                        type="file"
+                        accept="image/*"
+                        multiple
+                        onChange={handleImageChange}
+                        style={{ display: 'none' }}
+                        />
+                </div>
+                <div className={styles.tagsearch}>
+                    <input
+                        type="text"
+                        value={hashtag}
+                        onKeyDown={handleKeyDown}
+                        onChange={handleHashtagChange}
+                        placeholder='# 해시태그를 입력하세요'
+                        className={styles.taginput}
+                    />
+                <div>
+                {selectedHashtags.map((tag, index) => (
+                    <span key={index}>{tag}</span>
+                ))}
+                </div>
             </div>
-        <div>
-            {selectedHashtags.map((tag, index) => (
-                <span key={index}>{tag}</span>
-            ))}
-        </div>
-        <button onClick={handleUpload}>Upload</button>
+            </div>
+            <button className={styles.uploadbutton} onClick={handleUpload}>Upload</button>
+            </div>
         </div>
     );
 };
