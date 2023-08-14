6446e8ffd8fe6bbd6c1cbd7edceff0b00e530b87
Chae Huichan <eqholic1125@gmail.com>
2023-08-14 21:04:52 +0900

    Chore: S3 기본 설정

---
+++ backend/src/main/java/com/ssafy/s3/FileService.java
@@ -0,0 +1,18 @@
+package com.ssafy.s3;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@RequiredArgsConstructor
+public class FileService {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+}
