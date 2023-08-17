8760ca9bb145e828345b02750abc410809567833
박승희 <pshee0329@naver.com>
2023-08-18 03:53:11 +0900

    Feat: 지도페이지 왼쪽 컴포넌트, 방매물 리스트 변경

--- frontend/src/components/Maps/RoomList.js
+++ frontend/src/components/Maps/RoomList.js
@@ -1,42 +1,70 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './RoomList.module.css';
 import InfiniteScroll from 'react-infinite-scroll-component';
+import RoomListItem from './RoomListItem';
+import axios from 'axios';
 
 
-const RoomList = () => {
-    const [items, setItems] = useState(Array.from({length:20}))
-    const [hasMore, sethasMore] = useState(true)
+
+const RoomList = (props) => {
+    const [hasMore, sethasMore] = useState(true);
+    const [roomids, setRoomids] = useState([0,1,2,3,4,5,6,7,8]);
+    useEffect(() => {
+        if(!props.let || !props.lon || !props.word) return
+        const SearchByAddressRequestDto = {
+            "address": props.word,
+            "content": '',
+        };
+        const SearchByStationUnivRequestDto = {
+            "lat": props.lat,
+            "lon": props.lon,
+            "content": '',
+        };
+        if (props.lat === 'lat') {
+            axios.post(`${process.env.REACT_APP_API_ROOT}/roomdeal/search-address`, SearchByAddressRequestDto
+            ).then((response) => {
+                console.log("주소주소", response.data)
+                setRoomids(response.data.data)
+            }).catch((error) => {
+                console.error('API 호출 에러:', error);
+            })
+        }
+        else {
+            axios.post(`${process.env.REACT_APP_API_ROOT}/roomdeal/search-station-univ`, SearchByStationUnivRequestDto
+            ).then((response) => {
+                console.log("역역", response.data)
+                setRoomids(response.data.data)
+            }).catch((error) => {
+                console.error('API 호출 에러:', error);
+            })
+        }
+    }, [props.lat,props.lon,props.word]);
 
     const fetchMoreData = () =>{
-        if (items.length >= 80) {
+        if (roomids.length >= 80) {
             sethasMore(false)
             return
         } 
 
         setTimeout(()=>{
-            setItems(prevItems => prevItems.concat(Array.from({ length: 20 })))
+            setRoomids(prevItems => prevItems.concat(roomids))
         },1500)
     }
 
     return (
         <div className={styles.Frame}>
             <div className={styles.btnparent}>
-                <button className={styles.mybtn}>입주순</button>  
-                <button className={styles.mybtn}>가격순</button>  
-                <button className={styles.mybtn}>등록순</button>
             </div>
             <div id="scrollableDiv" className={styles.scrollableDiv}>
                 <InfiniteScroll
-                    dataLength={items.length}
+                    dataLength={roomids.length}
                     next={fetchMoreData}
                     hasMore={hasMore}
                     loader={<h4>불러오는 중이곰...</h4>}
                     scrollableTarget="scrollableDiv"
                 >
-                {items.map((i, index) => (
-                    <div className={styles.ListItems} key={index}>
-                        room - #{index}
-                    </div>
+                {roomids.map((value, index) => (
+                    <RoomListItem key={index} roomid={value}/>
                 ))}
                 </InfiniteScroll>
             </div>
