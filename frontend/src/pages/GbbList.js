5cda0ec9ed5bdbbb05ac0dcad2281fd9ee9a615c
박승희 <pshee0329@naver.com>
2023-08-17 09:27:41 +0900

    Design: 곰방봐 리스트 className 설정

--- frontend/src/pages/GbbList.js
+++ frontend/src/pages/GbbList.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import Header from '../components/Header';
-import { useNavigate } from 'react-router-dom';
-import styles from '../components/Gbb.module.css';
+import React, { useState } from "react";
+import Header from "../components/Header";
+import { useNavigate } from "react-router-dom";
+import styles from "../components/Gbb.module.css";
 
 const GbbList = ({ imageList }) => {
     const navigate = useNavigate();
-    const [searchText, setSearchText] = useState('');
+    const [searchText, setSearchText] = useState("");
     const [selectedTags, setSelectedTags] = useState([]);
 
     const [responseArticleId, setResponseArticleId] = useState(null); // 응답으로 받은 article_id
@@ -18,13 +18,13 @@ const GbbList = ({ imageList }) => {
             const requestBody = {
                 filter: searchText,
                 // search: searchText,
-                tags: selectedTags.map(tag => ({ tagname: tag }))
+                tags: selectedTags.map((tag) => ({ tagname: tag })),
             };
 
             const response = await fetch(`${apiUrl}/showroom/list`, {
-                method: 'POST',
+                method: "POST",
                 headers: {
-                    'Content-Type': 'application/json',
+                    "Content-Type": "application/json",
                 },
                 body: JSON.stringify(requestBody),
             });
@@ -34,48 +34,74 @@ const GbbList = ({ imageList }) => {
             // 응답 데이터 처리
             setResponseArticleId(data.article_id);
             setResponseRoomId(data.room_id);
-
         } catch (error) {
-            console.error('에러:', error);
+            console.error("에러:", error);
         }
     };
 
     return (
         <div>
             <Header />
-            <div className={styles.header}>
-                <div className={styles.h1}>당신의 <span className={styles.h2}>곰방</span>을 보여주세요 .</div>
-                <button onClick={() => navigate("/gbbcreate")} className={styles.plusbutton}>+</button>
+            <div className={styles.gbblist}>
+                <div className={styles.header}>
+                    <div className={styles.h1}>
+                        당신의 <span className={styles.h2}>곰방</span>을
+                        보여주세요.
+                    </div>
+                    <div>
+                        <button
+                            onClick={() => navigate("/gbbcreate")}
+                            className={styles.plusbutton}
+                        >
+                            +
+                        </button>
+                    </div>
+                </div>
+                <div className={styles.search}>
+                    <div className={styles.inputbox}>
+                        <div>
+                            <input
+                                className={styles.searchbox}
+                                value={searchText}
+                                onChange={(event) =>
+                                    setSearchText(event.target.value)
+                                }
+                                placeholder="보고싶은 지역을 입력하세요."
+                            />
+                        </div>
+                        <div>
+                            {selectedTags.map((tag, index) => (
+                                <span className={styles.tag} key={index}>
+                                    {tag}
+                                </span>
+                            ))}
+                        </div>
+                        <div>
+                            <input
+                                className={styles.searchbox}
+                                value={selectedTags}
+                                onChange={(event) =>
+                                    setSelectedTags(
+                                        event.target.value.split(",")
+                                    )
+                                }
+                                placeholder="해시태그를 입력하세요."
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <button onClick={handleSearch}>검색</button>
+                    </div>
+                </div>
+                {/* 응답으로 받은 데이터 표시 */}
+                {responseArticleId !== null && (
+                    <p>응답으로 받은 article_id: {responseArticleId}</p>
+                )}
+                {responseRoomId !== null && (
+                    <p>응답으로 받은 room_id: {responseRoomId}</p>
+                )}
+                {/* 이미지 리스트 출력 부분 */}
             </div>
-            <div className={styles.inputbox}>
-            <input
-                className={styles.searchbox}
-                value={searchText}
-                onChange={(event) => setSearchText(event.target.value)}
-                placeholder="보고싶은 지역을 입력하세요."
-            />
-            {selectedTags.map((tag, index) => (
-                <span key={index}>{tag}</span>
-            ))}
-            <input
-                className={styles.searchbox}
-                value={selectedTags}
-                onChange={(event) => setSelectedTags(event.target.value.split(','))}
-                placeholder="해시태그를 입력하세요."
-            />
-            <button className={styles.searchclick} onClick={handleSearch}>검색</button>
-            </div>
-            
-
-            {/* 응답으로 받은 데이터 표시 */}
-            {responseArticleId !== null && (
-                <p>응답으로 받은 article_id: {responseArticleId}</p>
-            )}
-            {responseRoomId !== null && (
-                <p>응답으로 받은 room_id: {responseRoomId}</p>
-            )}
-
-            {/* 이미지 리스트 출력 부분 */}
         </div>
     );
 };
