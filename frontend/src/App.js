3e1d4c6d0269dd083f9917d790606a813cd89091
박승희 <pshee0329@naver.com>
2023-08-18 04:09:09 +0900

    Feat: App.js 링크 생성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import Map from "./pages/MapPage";
-import Zzimlist from "./pages/ZzimList";
-import Roomout from "./pages/RoomOut";
-import RoomDetail from "./pages/RoomDetail";
+import Zzimlist from "./pages/Zimlist";
+import Roomout from "./pages/Roomout";
 import Signuplogin from "./pages/Signuplogin";
 import GbbList from "./pages/GbbList";
 import GbbCreate from "./pages/GbbCreate";
@@ -12,9 +11,12 @@ import AssigneeRtcRoom from "./components/Rtc/AssigneeRtcRoom";
 import GrantorRtcRoom from "./components/Rtc/GrantorRtcRoom";
 import Main from "./pages/Main";
 import Auth from "./pages/Auth";
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import RoomDetail from "./pages/RoomDetail";
+import RoomList from "./components/Maps/RoomList";
+import GbbDetail from "./pages/GbbDetail";
+import MyPage from "./pages/MyPage";
 
 const App = () => {
   const [imageList, setImageList] = useState([]);
@@ -28,14 +30,16 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/map" element={<Map />} />
+          <Route path="/map/:word/:lat/:lon" element={<Map />} />
           <Route path="/gbblist" element={<GbbList imageList={imageList} />} />
+          <Route path="/gbblist/:articleId" element={<GbbDetail/>} />
           <Route path="/gbbcreate" element={<GbbCreate onImageUpload={handleImageUpload} />} />
           <Route path="/zzim" element={<Zzimlist />} />
           <Route path="/roomout" element={<Roomout />} />
+          <Route path="/mypage" element={<MyPage />} />
           <Route path="/roomdetail/:roomDealid" element={<RoomDetail />} />
           <Route path="/login" element={<Signuplogin />} />
           <Route path="/auth" element={<Auth />} />
-          {/* <Route path="/test" element={<TestApp />} /> */}
           <Route path="/chatroom/:isGrantor/:id/:roomDealId" element={<ChatRoom />} />
           <Route path="/chatlist" element={<ChatList />} />
           <Route path="/rtcroom/grantor/:id/:roomDealId" element={<GrantorRtcRoom />} />
