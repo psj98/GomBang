4705f880f1bf454c102926f0851fc397d5c65e7a
Chae Huichan <eqholic1125@gmail.com>
2023-08-17 18:04:35 +0900

    Feat: myRoomDeal 반환 API 구현

--- backend/src/main/java/com/ssafy/roomDeal/repository/RoomDealRepository.java
+++ backend/src/main/java/com/ssafy/roomDeal/repository/RoomDealRepository.java
@@ -1,10 +1,13 @@
 package com.ssafy.roomDeal.repository;
 
+import com.ssafy.member.domain.Member;
 import com.ssafy.roomDeal.domain.RoomDeal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RoomDealRepository extends JpaRepository<RoomDeal, Long> {
-
+    List<RoomDeal> findAllByMember(Member member);
 }
