c79e8c7ac51988639fa8221e97e126fc0a98977a
HyundoLee <hyundo1995@gmail.com>
2023-08-18 10:06:30 +0900

    Design: 매물 필터 디자인 수정 최종

--- frontend/src/components/Main/RoomFilterForm.js
+++ frontend/src/components/Main/RoomFilterForm.js
@@ -1,26 +1,88 @@
 // import "bootstrap/dist/css/bootstrap.min.css";
 // import { Form } from "react-bootstrap";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./RoomFilterForm.module.css";
+import axios from 'axios';
+
+
 const RoomFilterForm = () => {
-  const [selectedOpt,setselectedOpt] = useState('전체')
-  const optionList = ['전체','원룸','오피스텔','빌라','아파트']
+  const navigate = useNavigate();
+  const [selectedOpt, setselectedOpt] = useState('전체')
+  const optionList = ['전체', '원룸', '오피스텔', '빌라', '아파트']
   const handleOptionClick = (option) => {
     setselectedOpt(option)
+  }
+  const [searchTerm, setSearchTerm] = useState('');
+  const [nearsearch, setNearsearch] = useState([]);
+  const [length, setLength] = useState([]);
+  const handleSearch = () => {
+    const SearchRelatedListRequestDto = {
+      "searchWord": searchTerm
+    }
+    console.log(searchTerm)
+    axios.post(`${process.env.REACT_APP_API_ROOT}/roomdeal/search-related-list`, SearchRelatedListRequestDto
+    ).then((response) => {
+      console.log(response)
+      setNearsearch(response.data.data)
+      setLength(response.data.data)
+    }).catch((error) => {
+      console.error('API 호출 에러:', error);
+    });
+  };
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleSearch();
+      e.preventDefault(); 
+    }
+  };
+  function handleonclick(word, type, lat, lon) {
+    if (type === 'address') {
+      localStorage.setItem('searchloc',word)
+      navigate(`/map/${word}/${"lat"}/${"lon"}`)
+    }
+    else if (type === 'station' || type === 'univ') {
+      navigate(`/map/${"word"}/${lat}/${lon}`)
+    }
   }
 
   return (
     <div className={styles.bbody}>
-      <div className={styles.parent}>
-        <div className={styles.head}>어떤 방을 찾으시나?</div>
-        <div className={styles.group}>
-          {optionList.map((value)=>(
-            <div key={value} className={`${styles.option} ${selectedOpt === value ? styles.selected : ''}`} onClick={()=>handleOptionClick(value)}>
-              {value}
+      <div className={styles.roomFilterBody}>
+        <div className={styles.parent}>
+          <div className={styles.head}>어떤 방을 찾으시나?</div>
+          <div className={styles.group}>
+            {optionList.map((value) => (
+              <div key={value} className={`${styles.option} ${selectedOpt === value ? styles.selected : ''}`} onClick={() => handleOptionClick(value)}>
+                {value}
+              </div>
+            ))}
+          </div>
+          <input
+            className={styles.myinput}
+            type="text"
+            placeholder="지역을 입력하세요"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            onKeyDown={handleKeyPress}
+          />
+          <div className={styles.searchResultScroll}>
+            <div className={styles.searchResultFlex}>
+              {length.length > 0 ? (<>
+                {nearsearch.map((value, index) => (
+                  <div key={index} className={styles.searchResultDiv}>
+                    <div onClick={() => handleonclick(value.searchWord, value.searchType, value.lat, value.lon)}>{value.searchWord}</div>
+                  </div>
+                ))
+                }
+              </>
+              ) : null}
+              {length.length === 0 && searchTerm.length > 1 ? (
+                <div className={styles.searchResultDiv}>검색결과가 없습니다.</div>
+              ):null}
             </div>
-          ))}
+          </div>
         </div>
-        <input className={styles.myinput} type="text" placeholder="지역을 입력하세요"/>
       </div>
     </div>
   );
--- frontend/src/components/Maps/FilterNav.js
+++ frontend/src/components/Maps/FilterNav.js
@@ -1,24 +1,53 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './FilterNav.module.css';
 import ModalPortal from '../ModalPortal'
 import FilterAdjust from './FilterAdjust'
-const FilterNav = () => {
+const FilterNav = (props) => {
   const [showmodal,setshowmodal] = useState(false)
+  const [location,setlocation] = useState(localStorage.getItem('searchloc')?localStorage.getItem('searchloc'):'')
+  const [content,setcontent] = useState(localStorage.getItem('searchcon')?localStorage.getItem('searchcon'):'')
+  
+
+  useEffect(()=>{
+    if(props.word){
+      setlocation(props.word)
+      const locsearch = document.querySelector("#locsearch")
+      locsearch.disabled = true
+    }
+  },[props.word])
+
+
   const handleOpen = (event) =>{
+    setkeywordTostorage()
     setshowmodal(true)
   }
+
   const handleClose = (event) =>{
     event.preventDefault();
     if(event.target === event.currentTarget)
     {
+      //필터검색 fetch 
       setshowmodal(false)
     }
   }
 
+  const handleKeyPress = (event) =>{
+    if(event.key.code === 13 || event.key === 'Enter'){
+      setkeywordTostorage()
+    }
+  }
+
+
+  function setkeywordTostorage(){
+    localStorage.setItem('searchloc',location)
+    localStorage.setItem('searchcon',content)
+    // ES 검색 fetch보내기
+  }
+
   return (
     <div className={styles.Frame}>
-      <input className={styles.LocSearch} type="text" placeholder='지역, 학교, 역으로 검색'/>
-      <input className={styles.ContSearch} type="text" placeholder='검색어를 입력하세요'/>
+      <input id='locsearch' className={styles.LocSearch} onChange={(e)=>{setlocation(e.target.value)}} type="text" value={location} placeholder='지역, 학교, 역으로 검색'/>
+      <input className={styles.ContSearch} onChange={(e)=>{setcontent(e.target.value)}} type="text" value={content} placeholder='검색어를 입력하세요' onKeyDown={handleKeyPress}/>
       <div className={styles.icon_parents} >
         <span onClick={handleOpen}>상세 필터</span>
         <img className={styles.icon_detail}  src="/assets/detail.svg" alt="" onClick={handleOpen}/>
